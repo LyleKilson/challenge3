e113c6ac118d4119c3a1e8299ddd27e4515dd68e
Lyle Kilson <lylekilsongithub@gmail.com>
2021-02-14 15:53:04 -0700

    simplified if staterments

--- script.js
+++ script.js
@@ -12,45 +12,29 @@ var numberCharArr = numberChar.split("");
 var lowercaseCharArr = lowercaseChar.split("");
 var uppercaseCharArr = uppercaseChar.split("");
 
-// Other Variables
-var passwordLength;
+// Main
+function generatePassword() {
+  //Input list for Variable Arrays
+  var passwordlist = [];
 
-//Input list for Variable Arrays
-var passwordlist = [];
+  // Write password to the #password input
+  var passwordLength = prompt("Your password may be between 8 and 128 characters. How many characters would you like your password to consist of?");
+    if (
+      passwordLength < 8 ||
+      passwordLength > 128 ||
+      passwordLength === null ||
+      passwordLength === ""
+    ) {
+      window.alert(
+        "Invalid entry. Please click the 'Generate Password' button and try again."
+      );
+      return;
 
-// Write password to the #password input
-var lengthInput = function() {
-  passwordLength = prompt("Your password may be between <strong>8 and 128</strong> characters. How many characters would you like your password to consist of?");
-  if (passwordLength === null || passwordLength === "") {
-    return;
+    } else {
+      console.log(passwordLength);
+      if (confirm("Would you like special characters included in your password?\n(e.g !@#$%^&*()_-+={}[];:'`~<,>.?/|)"));
     
-  } else if (passwordLength < 8 || passwordLength > 128) {
-    window.alert("Invalid entry. Please click the 'Generate Password' button and try again.");
-    return;
-
-  } else {
-    console.log(passwordLength);
-    passwordCriteria ();
     }
 }
 
-var passwordCriteria = function() {
-   if (confirm("Would you like special characters included in your password (e.g !@#$%^&*()_-+={}[];:'`~<,>.?/|)?")) {
-    passwordlist = passwordlist + specialCharArr;
-    console.log(passwordlist);
-   } 
-} 
-
-
-
-
-
-generateBtn.addEventListener("click", lengthInput);
-
-
   
-//var password = generatePassword();
-
-//var passwordText = document.querySelector("#password");
-
- // passwordText.value = password;

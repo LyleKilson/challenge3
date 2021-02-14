b2515303f658eadd50911e0eabb58da1cd86a0af
Lyle Kilson <lylekilsongithub@gmail.com>
2021-02-13 19:30:17 -0700

    Added variables and arrays

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-// Assignment code here
+// Static Variables
+var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.?/|";
+var numberChar = "123456789";
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+// Arrays
+var specialCharArr = specialChar.split("");
+var numberCharArr = numberChar.split("");
+var lowercaseCharArr = lowercaseChar.split("");
+var uppercaseCharArr = uppercaseChar.split("");
 
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+Function generatePassword() {
+  var passwordLength = prompt ("Your password may be between 8 and 128 characters. How many characters would you like your password to consist of?");
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Invalid entry. Please click the 'Generate Password' button to try again.");
+  }
+}
 
 // Write password to the #password input
 function writePassword() {
-
-    var passwordLength = prompt("Please select the lentgh of your password. Your Password may be between 8-128 characters long.");
-
-    if (passwordLength < 8) {
-        alert("This is not enough characters to create a sucure password.")
-    }
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
